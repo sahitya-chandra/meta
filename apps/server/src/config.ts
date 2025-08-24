fdb99be32cd9efc7ecebe8cddb73a065d51075e4
DiyanshuVortex <divyanshuchandra9027@gmail.com>
2025-08-24 15:30:19 +0530

    prettier

--- apps/server/src/config.ts
+++ apps/server/src/config.ts
@@ -2,6 +2,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT || 4000;
-export const AUTH_SECRET =
-  (process.env.AUTH_SECRET as string);
+export const AUTH_SECRET = process.env.AUTH_SECRET as string;
 export const CORS_ORIGIN = process.env.NEXT_ORIGIN || 'http://localhost:3000';
